338f4a77511d7523f34bd01e1213208c2e270ad1
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-23 17:39:33 +0530

    server and deploy update

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const contactusRouter = require('./routes/conactus');
 
@@ -13,13 +14,13 @@ server.use('/contactus',contactusRouter.router)
 main().catch(err => console.log(err));
 
 async function main() {
-  // await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  await mongoose.connect('mongodb+srv://DemoUser:demouser123@cluster0.03e2hgr.mongodb.net/chalhitsir');
+  // await mongoose.connect('mongodb://127.0.0.1:27017/');
+  await mongoose.connect(process.env.MONGODB_URL);
 
   console.log("connected to server");
 }
 
 
-server.listen(3001,()=>{
+server.listen(process.env.PORT,()=>{
     console.log('Server is running on port 3001');
 })
