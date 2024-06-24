d1eb16c1b069ba49cb95f2ae82338cbece937404
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-24 17:46:38 +0530

    markers added

--- Client/src/components/MapComponent.jsx
+++ Client/src/components/MapComponent.jsx
@@ -2,44 +2,75 @@ import React from 'react';
 import { useRef, useState, useEffect } from 'react';
 import { mappls, mappls_plugin } from  'mappls-web-maps';
 
-const auto = {
-  width: "300px",
-  position: "absolute",
-  zIndex: 999,
-  fontSize: "15px",
-  padding: "10px",
-  border: "1px solid #ddd",
-  outline: "none !important",
-};
 
-let apikey = '7783f393-2f77-4ddc-8d7b-0c20327a8005';
+let apikey = '6eb75952-7043-40d6-9e5f-36fb2a8eac33';
 const mapplsClassObject = new mappls();
 const mapplsPluginObject = new mappls_plugin();
+// console.log(mapplsClassObject);
 
 
-const PinmarkerPlugin = ({ map }) => {
-  const pinMarkerRef = useRef(null);
+const PinmarkersPlugin = ({ map }) => {
+  const pinMarkersRef = useRef(null);
+
+  let geoData = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          htmlPopup: `<h2 style = "color: #00698f">Heavy Traffic </h2>`,
+          icon: "https://apis.mapmyindia.com/map_v3/1.png",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [30.054570746528846, 78.22007074493854],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          htmlPopup: `<h2 style = "color: #00698f">Heavy Traffic </h2>`,
+          icon: "https://apis.mapmyindia.com/map_v3/1.png",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [30.116110691341547, 78.28309192626698],
+        },
+      },
+      {
+        type: "Feature",
+        properties: {
+          htmlPopup: `<h2 style = "color: #00698f">Landslide </h2>`,
+          icon: "https://apis.mapmyindia.com/map_v3/1.png",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [30.118718492663167, 78.37159409255548],
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
-    if (map && pinMarkerRef.current) {
-      pinMarkerRef.current.remove();
-      mapplsClassObject.removeLayer({ map, layer: pinMarkerRef.current });
+    if (map && pinMarkersRef.current) {
+      pinMarkersRef.current.remove();
+      mapplsClassObject.removeLayer({ map, layer: pinMarkersRef.current });
     }
 
-    pinMarkerRef.current = mapplsPluginObject.pinMarker(
+    pinMarkersRef.current = mapplsClassObject.addGeoJson(
       {
-        
-        map: map,
-        pin: ['qt9get',"1qtt9s",'7wwq18','1q2l5y'],
-        popupHtml: '<h2 style="color:red">Heavy Traffic</h2>',
-      },
+         map: map,
+         data: geoData, 
+         fitbounds: true, 
+         cType: 0 
+        },
       (e) => {
         console.log(e);
       }
     );
     return () => {
-      if (map && pinMarkerRef.current) {
-        mapplsClassObject.removeLayer({ map, layer: pinMarkerRef.current });
+      if (map && pinMarkersRef.current) {
+        mapplsClassObject.removeLayer({ map, layer: pinMarkersRef.current });
       }
     };
   }, [map]);
@@ -47,7 +78,7 @@ const PinmarkerPlugin = ({ map }) => {
 
 export default  function MapComponent() {
 
-  const  styleMap  = {width:  '90%', height:  '90vh', display:'inline-block'};
+  const  styleMap  = {width:  '100%', height:  '90vh', display:'inline-block'};
 
 
   const mapRef = useRef(null);
@@ -56,7 +87,10 @@ export default  function MapComponent() {
   useEffect(() => {
     mapplsClassObject.initialize(
       apikey,
-      { map: true, plugins: ["marker"] },
+      { 
+        map: true, 
+        plugins: ["marker"] 
+      },
       () => {
         if (mapRef.current) {
           mapRef.current.remove();
@@ -67,7 +101,7 @@ export default  function MapComponent() {
             //  //Properties Object
             center: [30.20, 78.04], // the coordinates as [lat, lon]
             
-            zoom: 10, //the initial Map `zoom` level.
+            zoom: 9, //the initial Map `zoom` level.
             },
         });
         mapRef.current.on("load", () => {
@@ -90,7 +124,7 @@ export default  function MapComponent() {
     <div  id="mapRef"  style={styleMap}>
     
     {
-    isMapLoaded && <PinmarkerPlugin map={mapRef.current}/>
+    isMapLoaded && <PinmarkersPlugin map={mapRef.current}/>
     }
     </div>
 
