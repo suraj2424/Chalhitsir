a3e7e0fff53d874200c37861b3818dd9195d3654
Suraj Suryawanshi <surajsuryawanshi2424@gmail.com>
2024-12-03 22:21:14 +0530

    minor index.js changes

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ main().catch((err) => console.log(err));
 
 async function main() {
   // await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  await mongoose.connect(DATABASE_URL);
+  await mongoose.connect(process.env.DATABASE_URL);
 
   console.log("connected to MongoDB");
 }
